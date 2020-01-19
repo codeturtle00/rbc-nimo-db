60f980ce211bd87009f929bdf091c3f5c9b4766a
Alex Shih <alex.alex.shih@gmail.com>
2020-01-19 02:51:37 -0500

    created gitignore and main

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
+    }
+
+}
