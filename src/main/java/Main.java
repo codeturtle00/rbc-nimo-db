6f11df9f9879533fc3a784a66e69817d5edeeb03
Alex Shih <alex.alex.shih@gmail.com>
2020-01-19 03:28:13 -0500

    setup

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,23 @@
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
 
+import java.net.UnknownHostException;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnknownHostException {
         MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
+
+        // Then
+        assertThat(mongoClient, is(notNullValue()));
+//        DB database = mongoClient.getDB("RBCPartners");
+//        DBCollection collection = database.getCollection("TheCollectionName");
+
     }
 
 }
